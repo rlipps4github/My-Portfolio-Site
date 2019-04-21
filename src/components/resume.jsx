3b47e49a485c38704ff33c09c1c3bca11ba0183d
Ron Lipps <ronlipps@rons-air.attlocal.net>
2019-04-20 22:53:54 -0700

    add theme styles more nav cleanup

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -7,9 +7,6 @@ class Resume extends Component {
         return (
             <div id="section-content" className="resume container">
                 <iframe id="resume" src="https://drive.google.com/file/d/1RfU0PUfTCeJyLDrFn8ibGgI8reJeRw-p/preview"></iframe>
-                <a className="button" href="https://drive.google.com/file/d/1RfU0PUfTCeJyLDrFn8ibGgI8reJeRw-p/view?usp=sharing" target="_blank">
-                    <i className="fas fa-download"></i>
-                </a>
             </div>
         )
     
