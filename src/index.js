183a4c8014dbd3495cf4600f4896f6a7229e0b63
Ron Lipps <ronlipps@rons-imac27.attlocal.net>
2019-05-16 11:41:40 -0700

    fix IE11

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import * as serviceWorker from './serviceWorker';
+import 'babel-polyfill'
 
-import MainWrapper from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/index.css'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<MainWrapper />, document.getElementById('root'));
+import MainWrapper from './App'
+
+ReactDOM.render(<MainWrapper />, document.getElementById('root'))
 
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
